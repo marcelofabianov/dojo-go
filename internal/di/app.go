772ecb5109f3eb3c60b2047e297003bc8f0e4d27
Marcelo Fabiano <marcelofabianov@gmail.com>
2025-09-23 20:37:18 -0300

    feat: implementar life cycle OnStart e OnStop do fx para gerir o server http

--- internal/di/app.go
+++ internal/di/app.go
@@ -1,6 +1,9 @@
 package di
 
 import (
+	"context"
+	"errors"
+	"log/slog"
 	"net/http"
 
 	"go.uber.org/fx"
@@ -16,6 +19,24 @@ func New() *fx.App {
 
 		//----
 
-		fx.Invoke(func(*http.Server) {}),
+		fx.Invoke(registerHooks),
 	)
+}
+
+func registerHooks(lc fx.Lifecycle, srv *http.Server, logger *slog.Logger) {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			logger.Info("starting http server", "addr", srv.Addr)
+			go func() {
+				if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+					logger.Error("failed to start http server", "error", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Info("stopping http server")
+			return srv.Shutdown(ctx)
+		},
+	})
 }
