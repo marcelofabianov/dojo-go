32099c9563c9170226bb2542fe30117fb73fd6ad
Marcelo Fabiano <marcelofabianov@gmail.com>
2025-09-23 20:05:41 -0300

    feat: implementar postgres course repository

--- internal/repository/postgres_course_repository.go
+++ internal/repository/postgres_course_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/marcelofabianov/fault"
 
 	"github.com/marcelofabianov/dojo-go/internal/model"
 	"github.com/marcelofabianov/dojo-go/internal/port"
@@ -18,6 +19,18 @@ func NewPostgresCourseRepository(db *sqlx.DB) port.CourseRepositoryPort {
 }
 
 func (r *PostgresCourseRepository) CreateCourse(ctx context.Context, course *model.Course) error {
+	query := `
+		INSERT INTO courses (id, title, description, created_at)
+		VALUES (:id, :title, :description, :created_at)
+	`
+
+	_, err := r.db.NamedExecContext(ctx, query, course)
+	if err != nil {
+		return fault.Wrap(err,
+			"failed to insert course into database",
+			fault.WithCode(fault.Internal),
+		)
+	}
 
 	return nil
 }
